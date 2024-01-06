442ab3607133be0faaca4a92a553f9d0a89d7bbd
nimankz <103257688+nimankz@users.noreply.github.com>
2024-01-07 00:41:21 +0330

    Update Main.kt
    oop complited on this commit

--- calculatorV1.2/src/main/kotlin/Main.kt
+++ calculatorV1.2/src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 package org.example
 
-class calculator(num1: Int, num2: Int){
-    fun sum(num1: Int, num2: Int): Int{return num1 + num2}
-    fun sub(num1: Int, num2: Int): Int{return num1-num2}
-    fun mult(num1: Int, num2: Int): Int{return num1*num2}
-    fun div(num1: Int, num2: Int): Int{return num1/num2}
+class calculator(var num1: Int, var num2: Int){
+    fun sum(): Int{return num1 + num2}
+    fun sub(): Int{return num1-num2}
+    fun mult(): Int{return num1*num2}
+    fun div(): Int{return num1/num2}
 }
 fun main() {
     var num1:Int?=null //these are our input numbers.
@@ -12,10 +12,10 @@ fun main() {
     num1= 12
     num2= 8
     val calc = calculator(num1,num2)
-    println("num1 = ${num1}, num2 = ${num2}")
-    println("sum is: ${calc.sum(num1,num2)}")
-    println("div is: ${calc.div(num1,num2)}")
-    println("mult is: ${calc.mult(num1,num2)}")
-    println("sub is: ${calc.sub(num1,num2)}")
+    println("num1 = ${calc.num1}, num2 = ${calc.num2}")
+    println("sum is: ${calc.sum()}")
+    println("div is: ${calc.div()}")
+    println("mult is: ${calc.mult()}")
+    println("sub is: ${calc.sub()}")
 
 }
